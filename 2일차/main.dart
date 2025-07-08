69eb58c244206a18e0d28b85d71006d6a12b2cbb
dodo <ddoaak@gmail.com>
2025-07-08 12:59:00 +0900

    2day

---
+++ 2일차/main.dart
@@ -0,0 +1,12 @@
+void main() {
+  String a = 'banana';
+
+  if (a == '1') {
+    print("object");
+  } else if (a == 'banana') {
+    print("banana");
+  } else
+    (a) {
+      print("no cases");
+    };
+}
